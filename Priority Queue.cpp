527c1442adecd42f54f7507ee322552737fbf0db
Zhao Wei <pkuzw@126.com>
2014-07-18 23:34:53 +0800

    Create Priority Queue.cpp
    实现《算法导论》P80最大值优先队列。

---
+++ Priority Queue.cpp
@@ -0,0 +1,20 @@
+///@breif 用最大堆思想实现最大值优先队列
+///@author zhaowei
+///@date 2014.07.18
+
+
+＃include<iostream>
+using namespace std;
+
+///@brief 返回最大值
+///@param arr 数组指针
+///@return 返回优先队列中的最大key值元素
+///@author zhaowei
+///@date 2014.07.18
+int Maximum(int* arr)
+{
+  return arr[0];
+}
+
+///@brief 去掉并返回队列中具有最大key值的元素
+///@param arr 数组指针
