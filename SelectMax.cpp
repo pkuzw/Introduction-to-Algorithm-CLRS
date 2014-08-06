2a4f2c568345a486862dd2165026e06e81441237
Zhao Wei <pkuzw@126.com>
2014-08-06 22:12:13 +0800

    找到数组中的最大数
    给定数组长度，数组中元素均为自然数，找出最大数。

---
+++ SelectMax.cpp
@@ -0,0 +1,41 @@
+﻿///@file 找数组中的最大元素
+///@author zhaowei
+///@date 2014.08.06
+
+#include<iostream>
+using namespace std;
+ 
+///@brief 找出数组中的最大元素
+///@param arr 数组指针
+///@param arr_len 数组长度
+///@return 返回数组中的最大元素
+///@version 1.0
+int SelectMax(int* arr, const int arr_len)
+{
+	int max = 0;
+	for(int i = 0; i < arr_len; i++)
+	{
+		if(arr[i] > max)
+			max = arr[i];
+	}
+	return max;
+}
+
+int main()
+{
+	cout << "输入数组的大小： ";
+	int arr_len = 0;
+	cin >> arr_len;
+
+	cout << "输入数组： ";
+	int* arr = new int[arr_len];
+	for(int i = 0; i < arr_len; i++)
+	{
+		cin >> arr[i];
+	}
+
+	cout << "数组中的最大数： ";
+	cout << SelectMax(arr, arr_len) << endl;
+	delete []arr;
+
+}
