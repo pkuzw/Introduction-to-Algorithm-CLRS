d2116c442047c106bd463d7f5b968b162d54cd17
Zhao Wei <pkuzw@126.com>
2014-08-06 22:29:33 +0800

    找出最小数
    给定自然数数组，其中元素不超过2147483648，找出最小数。

---
+++ SelectMin.cpp
@@ -0,0 +1,43 @@
+///@file 找出数组中的最小值
+///@author zhaowei
+///@date 2014.08.06
+///@version 1.0
+
+#include <iostream>
+using namespace std;
+
+///@brief 找出数组中的最小值
+///@param arr 数组指针
+///@param arr_len 数组长度
+///@return 返回数组中的最小值
+///@version 1.0
+///@author zhaowei
+///@date 2014.08.06
+int SelectMin(int* arr, const int arr_len)
+{
+	int min = 2147483647;
+	for(int i = 0; i < arr_len; i++)
+	{
+		if(min > arr[i])
+			min = arr[i];
+	}
+	return min;
+}
+
+int main()
+{
+	cout << "输入数组的大小： ";
+	int arr_len = 0;
+	cin >> arr_len;
+
+	cout << "输入数组： ";
+	int* arr = new int[arr_len];
+	for(int i = 0; i < arr_len; i++)
+	{
+		cin >> arr[i];
+	}
+
+	cout << "数组中的最小数： ";
+	cout << SelectMin(arr, arr_len) << endl;
+	delete []arr;
+}
