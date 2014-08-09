92be153db361f7f39c32f2e4b8726d33e411f4cb
Zhao Wei <pkuzw@126.com>
2014-08-10 00:22:03 +0800

    多余的.h文件
    Xcode在新建cpp文件时只有新建cpp class选项，所以会多一个.h文件

---
+++ Xcode First Blood.h
@@ -0,0 +1,14 @@
+//
+//  Xcode First Blood.h
+//  Introduction to Algorithm CLRS
+//
+//  Created by Zhao Wei on 14-8-10.
+//  Copyright (c) 2014年 Zhao Wei. All rights reserved.
+//
+
+#ifndef __Introduction_to_Algorithm_CLRS__Xcode_First_Blood__
+#define __Introduction_to_Algorithm_CLRS__Xcode_First_Blood__
+
+#include <iostream>
+
+#endif /* defined(__Introduction_to_Algorithm_CLRS__Xcode_First_Blood__) */
