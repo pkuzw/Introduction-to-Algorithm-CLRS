f7f2ad564354b7174c612ef29d801bfb52238188
Zhao Wei <pkuzw@126.com>
2014-08-10 00:20:29 +0800

    第一次在Xcode上写程序
    在MacBook上写程序，感觉萌萌哒！

---
+++ Xcode First Blood.cpp
@@ -0,0 +1,16 @@
+//
+//  Xcode First Time with GitHub.cpp
+//  Introduction to Algorithm CLRS
+//
+//  Created by Zhao Wei on 14-8-10.
+//  Copyright (c) 2014年 Zhao Wei. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "第一次用Xcode写程序，有点小兴奋呢！" << endl;
+    cout << "尝试用Xcode在MacBook上写好程序，然后用GitHub客户端提交。效果如何呢？还不错！" << endl;
+    return 0;
+}
