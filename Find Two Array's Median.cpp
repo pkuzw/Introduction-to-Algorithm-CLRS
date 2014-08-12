89954f0e3459fcabeaba8e44ac78f401c57655d7
Zhao Wei <pkuzw@126.com>
2014-08-13 01:21:46 +0800

    找出两个已经排好序的数组在合并后的中位数，时间复杂度为O(n)
    这道题在《算法导论》P113， 在LeetCode上也遇见过，叫做Median of Two Sorted
    Arrays。当时就因为这道题卡住，做了几天的LeetCode便没有继续了。这次一定搞定它！

---
+++ Find Two Array's Median.cpp
@@ -0,0 +1,15 @@
+﻿///@file 用O(n)的时间复杂度对两个已经排好序的数组找出其合并后的中位数
+///@author zhaowei
+///@date 2014.08.12 
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int arr1[10] = {10, 32, 31, 2233, 12, 52, 37, 21, 8, 189};
+	int arr2[10] = {329, 13, 432, 6, 345, 6, 88, 9044, 12, 2};
+
+	return 0;
+}
