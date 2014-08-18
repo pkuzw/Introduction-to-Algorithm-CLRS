0d77e0b39e729fa318a8045a1b07f402a11b9ce4
Zhao Wei <pkuzw@126.com>
2014-08-18 16:32:16 +0800

    完成链表的练习
    1. 能够在指定位置添加新结点或者在链表头添加新结点；
    2. 能够删除指定结点；
    3. 良好的交互界面。

--- List.cpp
+++ List.cpp
@@ -1,7 +1,7 @@
 ﻿///@file 编写链表的基本操作。包括借点查找、插入结点、删除结点
 ///@author zhaowei
-///@date 2014.08.16
-///@version 1.0
+///@date 2014.08.18
+///@version 1.2
 
 #include<iostream>
 using namespace std;
@@ -35,40 +35,41 @@ listNode* ListSearch(listNode* list_head, int key)
 }
 
 ///@brief 在链表首部插入结点
-///@param list_head 链表指针
+///@param list_head	链表指针
 ///@param new_node 新插入结点
+///@return 返回插入后的链表首部指针
 ///@author zhaowei
-///@date 2014.08.16
-///@version 1.0
-void ListInsertToHead(listNode* list_head, listNode* new_node)
+///@date 2014.08.18
+///@version 1.1
+listNode* ListInsertToHead(listNode* list_head, listNode* new_node)
 {
-	new_node->next = list_head;
-	new_node->prev = nullptr;
 	if(list_head != nullptr)
 	{
-		list_head->prev = new_node;
+		new_node->next = list_head;
+		list_head->prev = new_node;		
 	}
-
 	list_head = new_node;	
-	return ;
+	new_node->prev = nullptr;
+	
+	return list_head;
 }
 
 ///@brief 在指定结点前插入新结点
 ///@param list_head 链表指针
 ///@param new_node 新结点指针
 ///@param key 插入位置之后的结点键值，如果有多个相同键值的结点，则认为是最靠近链表首部的结点
-///@return 如果链表包含待插入结点的位置（即后一个结点的键值存在），返回true；否则返回false
+///@return 返回插入后的链表首部指针
 ///@author zhaowei
-///@date 2014.08.16
-///@version 1.0
-bool ListInsert(listNode* list_head, listNode* new_node, int key)
+///@date 2014.08.18
+///@version 1.1
+listNode* ListInsert(listNode* list_head, listNode* new_node, int key)
 {
 	listNode* x = list_head;
 	while(x != nullptr && key != x->val)
 		x = x->next;
 	
 	if(x == nullptr)
-		return false;
+		return list_head;
 
 	else
 	{
@@ -76,8 +77,8 @@ bool ListInsert(listNode* list_head, listNode* new_node, int key)
 		new_node->prev = x->prev;
 
 		x->prev = new_node;
-		new_node->prev->next = x;
-		return true;
+		new_node->prev->next = new_node;
+		return list_head;
 	}
 
 }
@@ -85,19 +86,110 @@ bool ListInsert(listNode* list_head, listNode* new_node, int key)
 ///@brief 删除链表中的结点
 ///@param list_head 链表指针
 ///@param delete_node 待删除的结点
+///@return 返回插入后的链表首部指针
 ///@author zhaowei
-///@date 2014.08.16
-///@version 1.0
-void ListDelete(listNode* list_head, listNode* delete_node)
+///@date 2014.08.18
+///@version 1.1
+listNode* ListDelete(listNode* list_head, listNode* delete_node)
 {
-	if(delete_node->prev != nullptr)
-		delete_node->prev->next = delete_node->next;
-	
+	if(delete_node == nullptr)
+		return list_head;
 	else
-		list_head = delete_node->next;
+	{
+		if(delete_node->prev != nullptr)
+			delete_node->prev->next = delete_node->next;
 
-	if(delete_node->next != nullptr)
-		delete_node->next->prev = delete_node->prev;
+		else
+			list_head = delete_node->next;
 
-	return;
+		if(delete_node->next != nullptr)
+			delete_node->next->prev = delete_node->prev;
+
+		return list_head;
+	}
+
+}
+
+int main()
+{	
+	listNode* list_head = nullptr;
+
+	int insert_or_delete = 0;
+	cout << "请选择插入结点或者删除结点（0：插入节点；1：删除结点）： ";
+	while(cin >> insert_or_delete)
+	{
+		if(insert_or_delete == 0)
+		{
+			cout << "输入想要插入的结点键值： ";
+			int insert_val = 0;
+			cin >> insert_val;
+
+			listNode* new_node = new listNode;
+			new_node->val = insert_val;
+			new_node->prev = nullptr;
+			new_node->next = nullptr;
+
+			int insert_method = 0;
+			cout << "选择插入的方法（0：插入链表最前端；1：插入指定键值结点的前端）： ";
+			cin >> insert_method;
+						
+			if(insert_method == 0)
+				list_head = ListInsertToHead(list_head, new_node);
+			else
+			{
+				int key = 0;
+				cout << "输入指定结点的键值： ";
+				cin >> key;
+				list_head = ListInsert(list_head, new_node, key);
+			}
+
+			cout << "当前链表中包含的结点： ";
+
+			listNode* list_index = list_head;
+			while(list_index != nullptr)
+			{
+				cout << list_index->val << " ";
+				list_index = list_index->next;
+			}
+			cout << endl;
+			cout << "--------------------------------------------------------------" << endl;
+			cout << endl;
+		}
+
+		else
+		{
+			cout << "输入想要删除的结点键值： ";
+			int delete_val = 0;
+			cin >> delete_val;
+
+			if(list_head == nullptr)
+			{
+				cout << "此链表为空。" << endl;
+				cout << "--------------------------------------------------------------" << endl;
+				cout << endl;
+				cout << "请选择插入结点或者删除结点（0：插入节点；1：删除结点）： ";
+				continue;
+			}
+
+			listNode* delete_node = ListSearch(list_head, delete_val);
+
+			if(delete_node == nullptr)
+				cout << "链表中不含此节点，请重新输入想要删除的结点。" << endl;
+			list_head = ListDelete(list_head, delete_node);
+
+			cout << "当前链表中包含的结点： ";
+
+			listNode* list_index = list_head;
+			while(list_index != nullptr)
+			{
+				cout << list_index->val << " ";
+				list_index = list_index->next;
+			}
+			cout << endl;
+			cout << "--------------------------------------------------------------" << endl;
+			cout << endl;
+		}
+		cout << "请选择插入结点或者删除结点（0：插入节点；1：删除结点）： ";
+	}
+	return 0;
 }
