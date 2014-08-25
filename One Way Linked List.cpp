22091dd5ee6d236b8334ac1b0fd1e9f369dad0e9
Zhao Wei <pkuzw@126.com>
2014-08-25 17:43:13 +0800

    完善单链表练习
    定义单链表结点结构体。

--- One Way Linked List.cpp
+++ One Way Linked List.cpp
@@ -3,6 +3,24 @@
 ///@date 2014.08.22
 ///@version 1.0
 
+struct listNode
+{
+    listNode* next;
+    int val;
+};
+
+///@brief 查找给定值到结点
+///@param list_head 链表首结点
+///@param key 待查找待结点键值
+///@return 如果找到相应待结点，则返回该结点的指针；否则返回空指针
+///@author zhaowei
+///@date 2014.08.25
+///@version 1.0
+listNode* ListSearch(listNode* list_head, int key)
+{
+    
+}
+
 #include <iostream>
 using namespace std;
 
